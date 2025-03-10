35ba74ebafd3f2e14c29bbfb995478b8f0beed3b
Serhii Didenko <srg.dd0@gmail.com>
2025-03-11 00:17:20 +0200

    fix: add redirects to next config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,27 @@ const nextConfig: NextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
+	async rewrites() {
+		return [
+			{
+				source: '/admin/:path*',
+				destination: '/admin/:path*', // Фейковий rewrite для блокування обробки /admin у Next
+			},
+			{
+				source: '/:path*',
+				destination: '/:path*', // Дефолтний маршрут
+			},
+		]
+	},
+	async redirects() {
+		return [
+			{
+				source: '/admin',
+				destination: '/admin/',
+				permanent: true,
+			},
+		]
+	},
 }
 
 export default nextConfig
