9500ded2888785ddfccab58954108d8e69e63e52
Serhii Didenko <srg.dd0@gmail.com>
2025-03-11 16:53:42 +0200

    chore: remove admin path from next config

--- next.config.ts
+++ next.config.ts
@@ -5,27 +5,6 @@ const nextConfig: NextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
-	async rewrites() {
-		return [
-			{
-				source: '/admin/:path*',
-				destination: '/admin/:path*',
-			},
-			{
-				source: '/:path*',
-				destination: '/:path*',
-			},
-		]
-	},
-	async redirects() {
-		return [
-			{
-				source: '/admin',
-				destination: '/admin/',
-				permanent: true,
-			},
-		]
-	},
 }
 
 export default nextConfig
