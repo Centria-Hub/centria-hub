3e9ce1a7be86d441006a86d169751ac6cbaf126a
Serhii Didenko <srg.dd0@gmail.com>
2025-03-11 13:57:47 +0200

    chore: change next conf

--- next.config.ts
+++ next.config.ts
@@ -5,27 +5,27 @@ const nextConfig: NextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
-	// async rewrites() {
-	// 	return [
-	// 		{
-	// 			source: '/admin/:path*',
-	// 			destination: '/admin/:path*',
-	// 		},
-	// 		{
-	// 			source: '/:path*',
-	// 			destination: '/:path*',
-	// 		},
-	// 	]
-	// },
-	// async redirects() {
-	// 	return [
-	// 		{
-	// 			source: '/admin',
-	// 			destination: '/admin/',
-	// 			permanent: true,
-	// 		},
-	// 	]
-	// },
+	async rewrites() {
+		return [
+			{
+				source: '/admin/:path*',
+				destination: '/admin/:path*',
+			},
+			{
+				source: '/:path*',
+				destination: '/:path*',
+			},
+		]
+	},
+	async redirects() {
+		return [
+			{
+				source: '/admin',
+				destination: '/admin/',
+				permanent: true,
+			},
+		]
+	},
 }
 
 export default nextConfig
