4e0341365c47a86c111d2c865a7430a25ce6f55b
Serhii Didenko <srg.dd0@gmail.com>
2025-03-17 17:54:00 +0200

    chore: add public url from the env process

--- lib/directus.ts
+++ lib/directus.ts
@@ -1,6 +1,6 @@
 import { createDirectus, rest } from '@directus/sdk'
 
-const directus = createDirectus('https://api.hub.solo-web.studio').with(
+const directus = createDirectus(process.env.PUBLIC_URL!).with(
 	rest({
 		onRequest: options => ({ ...options, cache: 'no-store' }),
 	})
