25b9d378e47e62dc21f6b2e2d2da868da29dd71d
Misato Seki <116722027+Misato-Seki@users.noreply.github.com>
2025-03-28 11:37:29 +0200

    feat: integration api for events page (#72)
    * feat: retrieve event data by id
    
    * feat: retrieve all event data

---
+++ components/AddToCalender.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { Button } from './ui/button'
+
+interface AddToCalenderProps {
+	title: string
+	start_date: string
+	end_date?: string
+	location_address?: string
+}
+
+const AddToCalender: React.FC<AddToCalenderProps> = ({
+	title,
+	start_date,
+	end_date,
+	location_address,
+}) => {
+	const addToGoogleCalendar = () => {
+		const eventName = encodeURIComponent(title)
+		const startDate = new Date(start_date)
+			.toISOString()
+			.replace(/-|:|\.\d+/g, '')
+
+		const endDate = end_date
+			? new Date(end_date).toISOString().replace(/-|:|\.\d+/g, '')
+			: startDate
+
+		const location = location_address
+			? `&location=${encodeURIComponent(location_address)}`
+			: ''
+
+		const googleCalenderURL = `https://www.google.com/calendar/render?action=TEMPLATE&text=${eventName}&dates=${startDate}/${endDate}${location}`
+
+		window.open(googleCalenderURL, '_blank')
+	}
+
+	return (
+		<Button
+			variant='centriaRed'
+			className='w-fit'
+			onClick={addToGoogleCalendar}
+		>
+			Add to Calender
+		</Button>
+	)
+}
+
+export default AddToCalender
